f22cb73ea54692e0db91a6ce54593927cfb2e2c3
Abu Bakar <siddique0153@gmail.com>
2024-02-13 11:26:08 +0600

    update file

---
+++ src/main/com/book1/controller/admin/book/DeleteBookServlet.java
@@ -0,0 +1,25 @@
+package com.book1.controller.admin.book;
+
+import com.book1.service.BookServices;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/admin/delete_book")
+public class DeleteBookServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    public DeleteBookServlet() {
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        BookServices bookServices = new BookServices(request, response);
+        bookServices.deleteBook();
+    }
+
+}
